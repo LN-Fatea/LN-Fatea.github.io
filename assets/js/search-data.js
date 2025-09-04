35dd63578701b0ae53bb78b508931dcdfaa3384d
LN-Fatea <154744049+LN-Fatea@users.noreply.github.com>
2025-09-04 21:17:56 +0000

    Deploying to gh-pages from @ LN-Fatea/LN-Fatea.github.io@f86906dd8b3d31add9050fc44c9043c780081cfb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -504,32 +504,18 @@ ninja.data = [{
           window.open("mailto:%6C%65%6F.%67%61%6F.%6A%7A@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/LN-Fatea", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/Leo_Neo_G", "_blank");
         },
       },{
       id: 'light-theme',
